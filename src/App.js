30daf2b1720170ea8448f0163f19ee261d875960
Yujie wU <ywu3-6>
2020-06-22 02:29:44 +1000

    updated header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 function App() {
+  // putting Header out of the Switch can make Header always is always presented and rendered.
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
